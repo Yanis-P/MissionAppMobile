caf55735c84cf1d4303a6f456abffb2f663510c8
palatete <yanisprime@hotmail.fr>
2026-03-13 13:55:42 +0100

    Initial commit

---
+++ Biblio/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblio
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
